3d2cc86de4dba2a4dd65a747997245e639e4b469
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-31 12:38:51 +1200

    Added tutorial Article code

--- src/mysite/code/ArticleHolder.php
+++ src/mysite/code/ArticleHolder.php
@@ -2,6 +2,7 @@
 
 class ArticleHolder extends Page {
     private static $allowed_children = array('ArticlePage');
+    private static $icon = "cms/images/treeicons/news-file.gif";
 }
 
 class ArticleHolder_Controller extends Page_Controller {
--- src/mysite/code/ArticlePage.php
+++ src/mysite/code/ArticlePage.php
@@ -1,10 +1,24 @@
 <?php
 
 class ArticlePage extends Page {
-        private static $db = array(
+    private static $icon = "cms/images/treeicons/news-file.gif";
+    private static $db = array(
         'Date' => 'Date',
         'Author' => 'Text'
     );
+
+    public function getCMSFields() {
+        $fields = parent::getCMSFields();
+
+        $dateField = new DateField('Date', 'Article Date (for example: 31/07/2014)');
+        $dateField->setConfig('showcalendar', true);
+        $dateField->setConfig('dateformat', 'dd/MM/YYYY');
+
+        $fields->addFieldToTab('Root.Main', $dateField, 'Content');
+        $fields->addFieldToTab('Root.Main', new TextField('Author', 'Author Name'), 'Content');
+
+        return $fields;
+    }
 }
 
 class ArticlePage_Controller extends Page_Controller {
