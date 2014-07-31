89869b7ab2fba169cd21fce2b9825e0de577339d
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-31 12:49:31 +1200

    Set up RSS feed

--- src/mysite/code/ArticleHolder.php
+++ src/mysite/code/ArticleHolder.php
@@ -1,9 +1,22 @@
 <?php
 
 class ArticleHolder extends Page {
+    public function init() {
+        RSSFeed::linkToFeed($this->Link() . "rss");
+        parent::init();
+    }
+
     private static $allowed_children = array('ArticlePage');
     private static $icon = "cms/images/treeicons/news-file.gif";
 }
 
 class ArticleHolder_Controller extends Page_Controller {
+    private static $allowed_actions = array(
+        'rss'
+    );
+
+    public function rss() {
+        $rss = new RSSFeed($this->Children(), $this->Link(), "Dem articles yo");
+        return $rss->outputToBrowser();
+    }
 }
